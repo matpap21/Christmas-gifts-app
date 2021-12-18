ebc65cfb1b64b9099da6bba7ffa0f92d3a0bfd5d
Mateusz <matpap21@wp.pl>
2021-12-18 15:44:27 +0100

    Init

--- src/main/java/pl/sda/christmasgifts/controller/ChristmasGiftsController.java
+++ src/main/java/pl/sda/christmasgifts/controller/ChristmasGiftsController.java
@@ -13,7 +13,6 @@ import pl.sda.christmasgifts.service.ChristmasGiftsService;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.net.http.HttpResponse;
 import java.util.Optional;
 import java.util.UUID;
 
